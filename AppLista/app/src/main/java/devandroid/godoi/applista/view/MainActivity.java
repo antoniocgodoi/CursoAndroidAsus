799097e7998290da69fc9bf5a9a119a96bb4f033
Antonio <antoniogodoi@gmail.com>
2024-01-01 19:30:04 -0300

    Versão 1 Clean Code

--- AppLista/app/src/main/java/devandroid/godoi/applista/view/MainActivity.java
+++ AppLista/app/src/main/java/devandroid/godoi/applista/view/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.godoi.applista.R;
 
@@ -11,20 +15,70 @@ import devandroid.godoi.applista.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
-    Pessoa pessoa1, pessoa2;
+    Pessoa pessoa;
 
+    EditText editTextPrimeiroNome;
+    EditText editTextSobrenome;
+    EditText editTextCurso;
+    EditText editTextTelefone;
+
+    Button botaoSalvar;
+    Button botaoLimpar;
+    Button botaoFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pessoa1 = new Pessoa("Antônio", "Godoi", "Android", 967631234);
-        pessoa2 = new Pessoa("José", "Feliciano", "Android", 1178847577);
+        pessoa = new Pessoa();
 
+        editTextPrimeiroNome = findViewById(R.id.editTextPrimeiroNome);
+        editTextSobrenome = findViewById(R.id.editTextSobrenome);
+        editTextCurso = findViewById(R.id.editTextCurso);
+        editTextTelefone = findViewById(R.id.editTextTelefone);
 
-        Log.i("Primeira pessoa", pessoa1.toString());
-        Log.i("Segunda pessoa", pessoa2.toString());
+        botaoLimpar = findViewById(R.id.botaoLimpar);
+        botaoSalvar = findViewById(R.id.botaoSalvar);
+        botaoFinalizar = findViewById(R.id.botaoFinalizar);
 
+        botaoLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextPrimeiroNome.setText("");
+                editTextSobrenome.setText("");
+                editTextCurso.setText("");
+                editTextTelefone.setText("");
+            }
+        });
+
+        botaoFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte Sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editTextPrimeiroNome.getText().toString().equals("")
+                        && !editTextSobrenome.getText().toString().equals("")
+                        && !editTextTelefone.getText().toString().equals("")
+                        && !editTextCurso.getText().toString().equals("")) {
+                    pessoa.setNome(editTextPrimeiroNome.getText().toString());
+                    pessoa.setSobrenome(editTextSobrenome.getText().toString());
+                    pessoa.setCurso(editTextCurso.getText().toString());
+                    pessoa.setTelefone(Integer.parseInt(editTextTelefone.getText().toString()));
+
+                    Toast.makeText(MainActivity.this, "Salvo: " + pessoa.toString(), Toast.LENGTH_LONG).show();
+                } else
+                    Toast.makeText(MainActivity.this, "Erro: Preencha Todos os Campos Primeiro", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        Log.i("Pessoa", pessoa.toString());
     }
 }
